fa443ff6aaa7118e2e3a1bd253c4bda43a801d83
/ Az1tchhh / <azamatbazarbai007@gmail.com>
2024-03-10 17:47:59 +0500

    first commit

---
+++ RealTimeParser/src/main/java/com/example/realtimeparser/RealTimeParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimeparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeParserApplication.class, args);
+    }
+
+}
---
+++ RealTimeParser/src/test/java/com/example/realtimeparser/RealTimeParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimeparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
